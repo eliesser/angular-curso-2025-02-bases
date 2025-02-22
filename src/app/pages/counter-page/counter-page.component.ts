d9e5c30bbd23ebaa82aacdf727487d26ae6b9e7f
Eliesser Freites <freiteseliesser@hotmail.com>
2025-02-22 23:01:13 +0100

    refactor: enhance counter component with OnPush change detection strategy

--- src/app/pages/counter-page/counter-page.component.ts
+++ src/app/pages/counter-page/counter-page.component.ts
@@ -1,13 +1,15 @@
-import { Component, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-counter',
   templateUrl: './counter-page.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CounterPageComponent {
   counter = 10;
-
   counterSignal = signal(10);
+
+  constructor() {}
 
   changeCounterBy(value: number) {
     this.counter += value;
@@ -16,7 +18,6 @@ export class CounterPageComponent {
 
   resetCounter() {
     this.counter = 0;
-
     this.counterSignal.set(0);
   }
 }
