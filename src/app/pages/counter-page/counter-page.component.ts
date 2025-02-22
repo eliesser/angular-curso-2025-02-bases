2e1605508b558b73550591d9e8793b96dd226410
Eliesser Freites <freiteseliesser@hotmail.com>
2025-02-22 22:53:30 +0100

    refactor: update counter logic to use signals and reset counter value

--- src/app/pages/counter-page/counter-page.component.ts
+++ src/app/pages/counter-page/counter-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-counter',
@@ -7,11 +7,16 @@ import { Component } from '@angular/core';
 export class CounterPageComponent {
   counter = 10;
 
+  counterSignal = signal(10);
+
   changeCounterBy(value: number) {
     this.counter += value;
+    this.counterSignal.update((prev) => prev + value);
   }
 
   resetCounter() {
-    this.counter = 10;
+    this.counter = 0;
+
+    this.counterSignal.set(0);
   }
 }
