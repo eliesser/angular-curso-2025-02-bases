29062e85bb6c73dfd7b01504231113354e01e12b
Eliesser Freites <freiteseliesser@hotmail.com>
2025-02-23 13:16:14 +0100

    feat: add Dragon Ball page component and integrate routing in navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { CounterPageComponent } from './pages/counter-page/counter-page.component';
 import { HeroPageComponent } from './pages/hero-page/hero-page.component';
+import { DragonBallPageComponent } from './pages/dragon-ball-page/dragon-ball-page.component';
 
 export const routes: Routes = [
   {
@@ -10,6 +11,10 @@ export const routes: Routes = [
   {
     path: 'hero',
     component: HeroPageComponent,
+  },
+  {
+    path: 'dragon-ball',
+    component: DragonBallPageComponent,
   },
   {
     path: '**',
---
+++ src/app/pages/dragon-ball-page/dragon-ball-page.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dragon-ball-page',
+  imports: [],
+  templateUrl: './dragon-ball-page.component.html',
+})
+export class DragonBallPageComponent {}
