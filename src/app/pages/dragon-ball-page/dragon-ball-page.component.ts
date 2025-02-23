44f9af1f1a687dc4b3ffad867116d406351e7ba1
Eliesser Freites <freiteseliesser@hotmail.com>
2025-02-23 13:24:15 +0100

    feat: update Dragon Ball page to display character list with power levels

--- src/app/pages/dragon-ball-page/dragon-ball-page.component.ts
+++ src/app/pages/dragon-ball-page/dragon-ball-page.component.ts
@@ -1,8 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+
+interface ICharacter {
+  id: number;
+  name: string;
+  power: number;
+}
 
 @Component({
   selector: 'app-dragon-ball-page',
   imports: [],
   templateUrl: './dragon-ball-page.component.html',
 })
-export class DragonBallPageComponent {}
+export class DragonBallPageComponent {
+  characters = signal<ICharacter[]>([
+    {
+      id: 1,
+      name: 'Goku',
+      power: 9001,
+    },
+    {
+      id: 1,
+      name: 'Vegeta',
+      power: 8000,
+    },
+    {
+      id: 1,
+      name: 'Piccoplo',
+      power: 3000,
+    },
+  ]);
+}
